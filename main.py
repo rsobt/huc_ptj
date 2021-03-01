caec13cfbc0edea369fe51d8c69a7dbb9570a538
iida <iida_t@eis.hokudai.ac.jp>
2021-03-01 15:47:59 +0900

    add logging, exception handling

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import json
 import pickle
 import tweepy
 import requests
+import settings_log
+import logging.config
 
 savefile = "list.pkl"
 page_nums = ["0", "1", "2"]
 label = {"0":"掲載日", "1":"企業名", "2": "担当者氏名", "3": "業種", "4": "所在地", "5": "電話番号", "100": "内容", "101": "勤務場所", "102": "勤務期間", "103": "勤務時間", "104": "賃金", "105": "募集人数", "106": "交通費", "107": "給与支払", "200": "採用の決め方", "201": "連絡事項", "202": "リンク先", "300": "詳細"}
+
+def get_logger(name):
+    logging.config.dictConfig(settings_log.LOGGING_CONF)
+    return logging.getLogger(name)
+
+logger = get_logger(__name__)
 
 def scrape_each(info_each):
     info_fix = info_each.find_all("dl")
@@ -35,7 +43,11 @@ def tweet(content):
 list_tod = []
 for page_num in page_nums:
     url = "https://www.hokudai-job.com/list.php?list_flag="  + page_num
-    r = requests.get(url, timeout=3.5)
+    try:
+        r = requests.get(url, timeout=3.5)
+    except Exception as e:
+        logger.error("Error at requests")
+        logger.exception("Raise Exception : %s", e)
     r.encoding = r.apparent_encoding
     soup = bs4.BeautifulSoup(r.text)
     
@@ -73,4 +85,10 @@ for detail in list_tweet:
         if len(detail[lab]) >= lim_len[lab]:
             content += "_"
         content += "\n"
-    tweet(content)
+    try:
+        tweet(content)
+        logger.info("success to tweet")
+        logger.info("msg : %s", content)
+    except Exception as e:
+        logger.error("Error at tweet")
+        logger.exception("Raise Exception : %s", e)
