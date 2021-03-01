7ca5e67b42dd1e091f2042efcb55e7d3a4173923
EC2 Default User <ec2-user@ip-10-0-0-62.ap-northeast-1.compute.internal>
2021-03-01 08:16:07 +0000

    charactor of environment to large from small

--- main.py
+++ main.py
@@ -28,10 +28,10 @@ def scrape_each(info_each):
     return dic
 
 def tweet(content):
-    consumer_key = os.environ["consumer_key"]
-    consumer_secret = os.environ["consumer_secret"]
-    access_token = os.environ["access_token"]
-    access_token_secret = os.environ["access_token_secret"]
+    consumer_key = os.environ["CONSUMER_KEY"]
+    consumer_secret = os.environ["CONSUMER_SECRET"]
+    access_token = os.environ["ACCESS_TOKEN"]
+    access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
